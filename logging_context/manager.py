88f1d06aae5c2943c4b5be1cfccc288ad0dc5bf7
Christian Hartung <hartung@live.com>
2022-02-16 18:50:48 -0300

    Update implementation to use a filter

--- logging_context/manager.py
+++ logging_context/manager.py
@@ -1,5 +1,3 @@
-import logging
-
 from .context import execution_context
 
 
@@ -8,17 +6,6 @@ class LoggingContext:
         self.data = data
 
     def __enter__(self):
-        current_record_factory = logging.getLogRecordFactory()
-
-        throwaway_record = current_record_factory(
-            __name__, logging.DEBUG, __file__, 14, "test_msg", [], None
-        )
-        if not hasattr(throwaway_record, "logging_context"):
-            from logging_context import logging as ctx_logging
-
-            new_factory = ctx_logging.log_record_factory(current_record_factory)
-            logging.setLogRecordFactory(new_factory)
-
         execution_context.push_context(self.data)
 
     def __exit__(self, *args, **kwargs):
--- tests/test_logging_context.py
+++ tests/test_logging_context.py
@@ -1,11 +1,17 @@
 import logging
 
-from logging_context import LoggingContext
+import pytest
 
+from logging_context import LoggingContext, LoggingContextFilter
 
-def test_context_adds_params(caplog):
+@pytest.fixture
+def logger():
     logger = logging.getLogger()
+    logger.addFilter(LoggingContextFilter())
+    yield logger
 
+
+def test_context_adds_params(caplog, logger):
     with caplog.at_level(logging.INFO):
         logger.info("before context")
         assert "region" not in caplog.records[-1].__dict__
@@ -16,9 +22,7 @@ def test_context_adds_params(caplog):
             assert caplog.records[-1].region == "br"
 
 
-def test_nested_contexts(caplog):
-    logger = logging.getLogger()
-
+def test_nested_contexts(caplog, logger):
     with caplog.at_level(logging.INFO):
         with LoggingContext(region="br"):
             logger.info("outer context")
@@ -39,9 +43,7 @@ def test_nested_contexts(caplog):
             assert "request_id" not in caplog.records[-1].__dict__
 
 
-def test_nested_context_override(caplog):
-    logger = logging.getLogger()
-
+def test_nested_context_override(caplog, logger):
     with caplog.at_level(logging.INFO):
         with LoggingContext(region="br"):
             logger.info("outer context")
@@ -57,3 +59,15 @@ def test_nested_context_override(caplog):
             logger.info("outer context")
             assert "region" in caplog.records[-1].__dict__
             assert caplog.records[-1].region == "br"
+
+
+def test_explicit_logging_overrides_context(caplog, logger):
+    with caplog.at_level(logging.INFO):
+        with LoggingContext(region="br"):
+            logger.info("should use context")
+            assert "region" in caplog.records[-1].__dict__
+            assert caplog.records[-1].region == "br"
+
+            logger.info("should use context", extra={"region": "us"})
+            assert "region" in caplog.records[-1].__dict__
+            assert caplog.records[-1].region == "us"
